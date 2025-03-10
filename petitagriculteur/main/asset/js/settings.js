ec9006c2379ce8c676409d0dcd294ed9facd6ab0
Émilie Guth <emilie.guth@gmail.com>
2025-03-10 20:27:33 +0100

    :art: Amélioration du scrollTo dans les setting

--- petitagriculteur/main/asset/js/settings.js
+++ petitagriculteur/main/asset/js/settings.js
@@ -12,6 +12,10 @@ class Settings {
             const { top: divTop } = qs('#account-list [name="account-' + id + '"]').getBoundingClientRect();
 
             window.scrollTo({top: divTop - mainTop, behavior: 'smooth'});
+
+            qsa('tr[name^="account"]', node => node.classList.remove('row-highlight'));
+            qs('tr[name="account-' + id + '"]').classList.add('row-highlight');
+
         }
     }
 }
